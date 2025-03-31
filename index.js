9ce23cdf5c01d5e5e4f5caf2d0210c617274fb08
Larry Otieno <larryotieno51@gmail.com>
2025-03-31 11:41:36 +0300

    Adds class extension lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+class Polygon {
+    constructor(sides) {
+        this.sides = sides;
+    }
+
+    get countSides() {
+        return this.sides.length;
+    }
+
+    get perimeter() {
+        return this.sides.reduce((acc, side) => acc + side, 0);
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        if (this.countSides !== 3) return false;
+        const [a, b, c] = this.sides;
+        return a + b > c && a + c > b && b + c > a;
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        return this.countSides === 4 && this.sides.every(side => side === this.sides[0]);
+    }
+
+    get area() {
+        return this.sides[0] ** 2;
+    }
+}
